fe91946ce5063197b116d9a91490ee64c7201b9c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-25 22:20:13 -0700

    Update ETA

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ $(() => {
 
 function setInitialArrival() {
   for(let i = 0; i < 3; i++) {
-    const arrivalElement = createArrivalElement(getDestination())
+    const eta = i === 0 ? "Now" : `${i*3}min`
+    const arrivalElement = createArrivalElement(getDestination(), eta)
     arrivalElement.css("top", `${i*80}px`)
     $("#schedule").append(arrivalElement)
   }
@@ -24,24 +25,27 @@ function update() {
 
 function slideUp() {
   $(".arrival").first().remove()
+
+  $(".arrival").first().children(".eta").text("Now")
+  $(".arrival").last().children(".eta").text("3min")
   $(".arrival").first().animate({top: `0px`}, 500)
   $(".arrival").last().animate({top: `80px`}, 500, insertNewArrival)
 }
 
 function insertNewArrival() {
-  const arrivalElement = createArrivalElement(getDestination())
+  const arrivalElement = createArrivalElement(getDestination(), "6min")
   arrivalElement.css("top", `160px`)
   arrivalElement.css("left", `-900px`)
   $("#schedule").append(arrivalElement)
   $(".arrival").last().animate({left: `0px`}, 500)
 }
 
-function createArrivalElement(destination) {
+function createArrivalElement(destination, eta) {
   const arrivalElement = $(`
     <div class="arrival">
       <img class="train-icon" src="train.png">
       <div class="destination">${destination}</div>
-      <div class="eta">5min</div>
+      <div class="eta">${eta}</div>
     </div>
   `)
   return arrivalElement
